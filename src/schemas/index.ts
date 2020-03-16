f44daea791256f41a5373a5372b83ab4ffb5421a
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-03-16 16:07:41 +0700

    Added files

---
+++ src/schemas/index.ts
@@ -0,0 +1,78 @@
+import TTx from './TTx.json'
+import IAliasTransaction from './IAliasTransaction.json'
+import IIssueTransaction from './IIssueTransaction.json'
+import ITransferTransaction from './ITransferTransaction.json'
+import IReissueTransaction from './IReissueTransaction.json'
+import IBurnTransaction from './IBurnTransaction.json'
+import IExchangeTransaction from './IExchangeTransaction.json'
+import ILeaseTransaction from './ILeaseTransaction.json'
+import ICancelLeaseTransaction from './ICancelLeaseTransaction.json'
+import IMassTransferTransaction from './IMassTransferTransaction.json'
+import ISetScriptTransaction from './ISetScriptTransaction.json'
+import ISetAssetScriptTransaction from './ISetAssetScriptTransaction.json'
+import IDataTransaction from './IDataTransaction.json'
+import ISponsorshipTransaction from './ISponsorshipTransaction.json'
+import IInvokeScriptTransaction from './IInvokeScriptTransaction.json'
+import IOrder from './IOrder.json'
+import IOrderParams from './IOrderParams.json'
+import ICancelOrder from './ICancelOrder.json'
+import ICancelOrderParams from './ICancelOrderParams.json'
+import IAliasParams from './IAliasParams.json'
+import IIssueParams from './IIssueParams.json'
+import ITransferParams from './ITransferParams.json'
+import IReissueParams from './IReissueParams.json'
+import IBurnParams from './IBurnParams.json'
+import ILeaseParams from './ILeaseParams.json'
+import ICancelLeaseParams from './ICancelLeaseParams.json'
+import IMassTransferParams from './IMassTransferParams.json'
+import ISetScriptParams from './ISetScriptParams.json'
+import ISetAssetScriptParams from './ISetAssetScriptParams.json'
+import IDataParams from './IDataParams.json'
+import ISponsorshipParams from './ISponsorshipParams.json'
+import IInvokeScriptParams from './IInvokeScriptParams.json'
+import IInvokeScriptPayment from './IInvokeScriptPayment.json'
+import IInvokeScriptCall from './IInvokeScriptCall.json'
+import INodeRequestOptions from './INodeRequestOptions.json'
+import TSeedTypes from './TSeedTypes.json'
+import WithId from './WithId.json'
+import WithTxType from './WithTxType.json'
+export default {
+  TTx,
+  IAliasTransaction,
+  IIssueTransaction,
+  ITransferTransaction,
+  IReissueTransaction,
+  IBurnTransaction,
+  IExchangeTransaction,
+  ILeaseTransaction,
+  ICancelLeaseTransaction,
+  IMassTransferTransaction,
+  ISetScriptTransaction,
+  ISetAssetScriptTransaction,
+  IDataTransaction,
+  ISponsorshipTransaction,
+  IInvokeScriptTransaction,
+  IOrder,
+  IOrderParams,
+  ICancelOrder,
+  ICancelOrderParams,
+  IAliasParams,
+  IIssueParams,
+  ITransferParams,
+  IReissueParams,
+  IBurnParams,
+  ILeaseParams,
+  ICancelLeaseParams,
+  IMassTransferParams,
+  ISetScriptParams,
+  ISetAssetScriptParams,
+  IDataParams,
+  ISponsorshipParams,
+  IInvokeScriptParams,
+  IInvokeScriptPayment,
+  IInvokeScriptCall,
+  INodeRequestOptions,
+  TSeedTypes,
+  WithId,
+  WithTxType
+}
